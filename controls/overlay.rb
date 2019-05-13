9e688e12bba8a8692e39136362e6397429e47eec
Haynes Jr <dhaynes@mitre.org>
2019-05-13 09:49:28 -0400

    updated overlay and spreadsheet based on EA feedback.

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -2,11 +2,14 @@
 
 include_controls 'oracle-java-runtime-environment-7-unix-stig-baseline' do
   control 'V-32828' do
+    tag "cci": ['CCI-001162']
+    tag "nist": ['SC-18', 'Rev_4']
 
   end
 
   control 'V-32829' do
-
+    tag "cci": ['CCI-001162']
+    tag "nist": ['SC-18', 'Rev_4']
   end
 end
 
